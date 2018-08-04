677dc0f8c5d6d8e09225653fe8113e829d5cb75f
ravensorb <sanderson@eye-catcher.com>
2018-08-04 13:29:51 -0400

    Refactoring structure Setup cake build process Added nuspec file

---
+++ src/Xml2CSharp/Xml2CSharpConverer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace Xml2CSharp
+{
+    public class Xml2CSharpConverer
+    {
+        public IEnumerable<Class> Convert(string xml, string customNameSpace = null)
+        {
+            var xElement = XElement.Parse(xml);
+
+            return xElement.ExtractClassInfo(customNameSpace);
+        }
+
+    }
+}
